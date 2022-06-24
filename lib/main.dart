e4b01ec4df180c76c46fb9f3d4f9789047b9f5a4
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-24 22:53:47 +0100

    Added a minheight to the word box

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,36 @@ class HomePage extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(width: double.infinity, height: 50),
+            const SizedBox(width: double.infinity, height: 50),
             Container(
-              color: Colors.orangeAccent,
-              height: 200,
+              constraints: BoxConstraints(
+                minHeight: 100,
+              ),
               width: 350,
-              child: const Text("fgh")
-            )
+              color: Colors.orangeAccent,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Praxeologies",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "The study of human action and conduct",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              )
+            ),
           ],
         )
       ),
