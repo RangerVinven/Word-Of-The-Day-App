f4eb281613dba5586db050ddf8dc4f09aa15399a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-29 22:23:14 +0100

    Added a getFlashcardsToShow method

--- lib/services/FlashcardService.dart
+++ lib/services/FlashcardService.dart
@@ -6,9 +6,11 @@ import 'package:word_of_the_day/models/Flashcard.dart';
 
 class FlashcardService {
   late Box flashcardBox;
+  late List<Flashcard> flashcards;
 
   FlashcardService() {
     flashcardBox = Hive.box("flashcardBox");
+    flashcards = getFlashcardsToShow();
   }
 
   void addFlashcard(Word word, DateTime? reviewDate) {
@@ -18,7 +20,7 @@ class FlashcardService {
   // Changes the dateToReview variable
   void updateFlashcard(Word word, bool gotCorrect) {
     // Loops through the flashcards
-    for (var i = 0; i < flashcardBox.length; i++) {
+    for (var i = 0; i < flashcards.length; i++) {
       Flashcard flashcard = flashcardBox.getAt(1);
 
       // If the flashcard is the one currently showing, change the reviewBy date
@@ -33,6 +35,21 @@ class FlashcardService {
         print(flashcard.word.word);
       }
     }
+  }
+
+  List<Flashcard> getFlashcardsToShow() {
+    List<Flashcard> flashcards = [];
+    DateTime now = DateTime.now();
+
+    for (var i = 0; i < flashcardBox.length; i++) {
+      Flashcard flashcard = flashcardBox.getAt(i);
+
+      if(flashcard.dateToReview.day == now.day && flashcard.dateToReview.month == now.month && flashcard.dateToReview.year == now.year) {
+        flashcards.add(flashcard);
+      }
+    }
+
+    return flashcards;
   }
 
 }
